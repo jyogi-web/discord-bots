957486447585238422ec4bbacaca7ad9cf1075cb
KOU050223 <kouhei20050223@icloud.com>
2026-04-10 03:08:58 +0900

    fix: nixからhaskell周りを削除
    Closes #11

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "discord-bots - Node.js bots + Haskell services";
+  description = "discord-bots - Node.js bots";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -10,32 +10,18 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        haskellPkgs = pkgs.haskell.packages.ghc984;
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             # Node.js (bots/) - npmはNode.jsに同梱
             pkgs.nodejs_24
-
-            # GHC & build tools (services/text-gacha/)
-            haskellPkgs.ghc
-            haskellPkgs.cabal-install
-            haskellPkgs.haskell-language-server
-            haskellPkgs.hlint
-            haskellPkgs.ormolu
-
-            # System libraries
-            pkgs.zlib
-            pkgs.pkg-config
           ];
 
           shellHook = ''
             echo "discord-bots dev environment"
             echo "Node: $(node --version)"
             echo "npm:  $(npm --version)"
-            echo "GHC:  $(ghc --version)"
-            echo "Cabal: $(cabal --version | head -1)"
           '';
         };
       });
